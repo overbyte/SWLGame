ca114cf22302492a946c10cda06b303acb66c77b
Allandt Bik-Elliott <allandt@gmail.com>
2023-02-15 23:46:55 +0000

    use joystick analog strength for inputs, update animation tree blend to favour up and down facing when walking diagonally, fix animation looping by setting correct length

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -2,7 +2,28 @@ extends CharacterBody2D
 
 @export var speed: float = 80.0
 
-func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+
+func _physics_process(_delta):
+	#var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#velocity = direction * speed
+	var direction := Vector2(
+		Input.get_action_strength("ui_right") -
+		Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") -
+		Input.get_action_strength("ui_up")
+	)
+
+	if direction.length() > 2:
+		direction = direction.normalized()
+
 	velocity = direction * speed
-	move_and_slide()
+
+	if velocity == Vector2.ZERO:
+		animationState.travel("Idle")
+	else:
+		animationState.travel("Walk")
+		animationTree.set("parameters/Idle/blend_position", velocity)
+		animationTree.set("parameters/Walk/blend_position", velocity)
+		move_and_slide()
